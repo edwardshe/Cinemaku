2e4362e5d8c38edf10d57e1af26289a903a3ac33
seangao <seancty@gmail.com>
2015-11-14 15:47:13 -0500

    modified movie.php to handle new Clarifai class

--- movie.php
+++ movie.php
@@ -7,10 +7,11 @@
 
     $auth = Clarifai::get_auth($client_id, $client_secret);
 
-    $query = new Search("The Social Network");
-    $query_tags = $query->get_tags($auth);
+    $query_name = "Step Brothers";
+    $query = new Search($query_name);
+    $query_links = $query->get_links();
 
-	$ch_scrape = curl_init('https://www.tastekid.com/movies/like/Step-Brothers');
+	$ch_scrape = curl_init('https://www.tastekid.com/movies/like/' . urlencode($query_name));
 	curl_setopt($ch_scrape, CURLOPT_RETURNTRANSFER, true);
 	//curl_setopt(... other options you want...)
 
@@ -25,19 +26,26 @@
 	if($status == 200)
 	{
 		preg_match_all('/<span class="tk-Resource-title">(.*)<\/span>/', $html, $matches);
-		$matches = array_slice($matches[0], 1, 5);
+		$matches = array_slice($matches[0], 1, 10);
 	}
 	else
 	{
 		echo "Connection failed.";
 	}
 
-    $results = array();
+    $links = array();
+    $links[$query_name] = $query_links;
     foreach ($matches as $match)
     {
         $match_search = new Search($match);
-        $tags = $match_search->get_tags($auth);
-        $results[$match] = sizeof(array_intersect($query_tags, $tags));
+        $links[$match] = $match_search->get_links();
+        //$tags = $match_search->get_tags($auth);
+        //$results[$match] = sizeof(array_intersect($query_tags, $tags));
+    }
+    $tags_array = Clarifai::get_multi_tags($links, $auth);
+    $results = array();
+    foreach (array_slice($tags_array, 1, 10) as $key => $tags) {
+        $results[$key] = count(array_intersect($tags, $tags_array[$query_name]));
     }
     arsort($results);
     var_dump($results);
