6afd9d6ba10a35cf8bcf9be3c6acc4e32bd90ba6
seangao <seancty@gmail.com>
2015-11-14 03:20:20 -0500

    encapsulated into GImages class

--- images.php
+++ images.php
@@ -1,18 +1,25 @@
 <?php
-$url = "https://ajax.googleapis.com/ajax/services/search/images?" .
-       "v=1.0&q=step%20brothers";
+class GImages {
+	private $results = array();
+	public function __construct($query) {
+		$url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q="
+		. urlencode($query);
+		$ch = curl_init();
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$body = curl_exec($ch);
-curl_close($ch);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$body = curl_exec($ch);
+		curl_close($ch);
 
-$data = json_decode($body);
+		$data = json_decode($body);
 
-foreach ($data->responseData->results as $result) {
-    $results[] = array('url' => $result->url, 'alt' => $result->title);
+		foreach ($data->responseData->results as $result) {
+		    $results[] = $result->url;
+		}
+	}
+
+	public function get_links() {
+		return $this->results;
+	}
 }
-
-print_r($results);
 ?>
